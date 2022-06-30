c8f7d3a905d064c7a31f361a02acc97f1201cb75
21A91A6104 <98638898+21A91A6104@users.noreply.github.com>
2022-06-30 09:52:25 +0530

    def prime(n):     count=0     for i in range(2,n):         if n%i==0:             count=1             return False     if count==0:         return True a=int(input()) b=int(input()) for i in range(1,100):     c=a+b+i     if prime(c):         print(i)         break

--- Battle_of_Primes.py
+++ Battle_of_Primes.py
@@ -1,8 +1,10 @@
 def prime(n):
+    count=0
     for i in range(2,n):
         if n%i==0:
+            count=1
             return False
-    else:
+    if count==0:
         return True
 a=int(input())
 b=int(input())
