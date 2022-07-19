7dfa9fb3496fc5ebd8a63a62bb3a4d370f61ead6
21A91A6104 <98638898+21A91A6104@users.noreply.github.com>
2022-07-19 17:59:23 +0530

    x=int(input()) for j in range(x):     k=0     a,b,c,d=map(int,input().split())     for i in range(1,a+1):         if(i%b==0 and i%c!=0):             k+=1         elif(i%b!=0 and i%c==0):             k+=1         if(k==d):             print("Win")             break     else:         print("Lose")

---
+++ Appy_and_Contest.py
@@ -0,0 +1,14 @@
+x=int(input())
+for j in range(x):
+    k=0
+    a,b,c,d=map(int,input().split())
+    for i in range(1,a+1):
+        if(i%b==0 and i%c!=0):
+            k+=1
+        elif(i%b!=0 and i%c==0):
+            k+=1
+        if(k==d):
+            print("Win")
+            break
+    else:
+        print("Lose")
